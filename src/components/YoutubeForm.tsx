6fb3ea5d1c6916cfd6b9db002470d7a7764c6352
Alireza <tafreshi13@gmail.com>
2023-12-21 23:50:31 +0330

    Register fields

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -2,6 +2,7 @@ import {useForm} from "react-hook-form";
 
 export const YouTubeForm = () => {
     const form = useForm();
+    const { register } = form;
 
     return (
         <div>
@@ -9,13 +10,13 @@ export const YouTubeForm = () => {
 
             <form>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" />
+                <input type="text" id="username" {...register("username")} />
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" name="email" />
+                <input type="email" id="email" {...register("email")} />
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name="channel" />
+                <input type="text" id="channel" {...register("channel")} />
 
                 <button>Submit</button>
             </form>
