5b41be72763b1db75659dc739fe3f9c29778f22e
Alireza <tafreshi13@gmail.com>
2024-01-04 19:23:33 +0330

    Display error messages

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,5 +1,5 @@
 import {useForm} from "react-hook-form";
-import { DevTool } from "@hookform/devtools";
+import {DevTool} from "@hookform/devtools";
 
 type FormValues = {
     username: string;
@@ -9,7 +9,7 @@ type FormValues = {
 
 export const YouTubeForm = () => {
     const form = useForm<FormValues>();
-    const { register, control, handleSubmit } = form;
+    const {register, control, handleSubmit, formState: {errors}} = form;
 
     const onSubmit = (data: FormValues) => {
         console.log("Form submitted", data);
@@ -20,28 +20,38 @@ export const YouTubeForm = () => {
             <h1>YouTube Form</h1>
 
             <form noValidate onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register("username", {
-                    required: "نام کاربری الزامی است"
-                })} />
+                <div className="form-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" {...register("username", {
+                        required: "نام کاربری الزامی است"
+                    })} />
+                    <p className="error">{errors.username?.message}</p>
+                </div>
 
-                <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register("email", {
-                    pattern: {
-                        value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                        message: "invalid format"
-                    }
-                })} />
+                <div className="form-control">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" id="email" {...register("email", {
+                        required:"ایمیل الزامی است",
+                        pattern: {
+                            value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                            message: "invalid format"
+                        }
+                    })} />
+                    <p className="error">{errors.email?.message}</p>
+                </div>
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register("channel", {
-                    required: "نام کانال الزامی است"
-                })} />
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input type="text" id="channel" {...register("channel", {
+                        required: "نام کانال الزامی است"
+                    })} />
+                    <p className="error">{errors.channel?.message}</p>
+                </div>
 
                 <button>Submit</button>
             </form>
 
-            <DevTool control={control} />
+            <DevTool control={control}/>
         </div>
     );
 };
