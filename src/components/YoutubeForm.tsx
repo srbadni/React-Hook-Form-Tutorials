59f256dee4327826e9be233e841770e2e8588354
Alireza <tafreshi13@gmail.com>
2024-01-04 15:51:56 +0330

    Add DevTool support

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,8 +1,9 @@
 import {useForm} from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export const YouTubeForm = () => {
     const form = useForm();
-    const { register } = form;
+    const { register, control } = form;
 
     return (
         <div>
@@ -20,6 +21,8 @@ export const YouTubeForm = () => {
 
                 <button>Submit</button>
             </form>
+
+            <DevTool control={control} />
         </div>
     );
 };
